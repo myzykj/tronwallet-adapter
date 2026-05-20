6dc1578b326310281e5bc97fb26af5875386c55f
Zorro <zorro11639@gmail.com>
2026-05-20 12:25:46 +0800

    chore: remove nvm in setup-e2e.js

--- scripts/sync-e2e.js
+++ scripts/sync-e2e.js
@@ -6,7 +6,7 @@ const root = join(__dirname, '..');
 const e2eDir = join(root, 'e2e');
 const repoUrl = 'https://github.com/zorro11639/tronwallet-adapter-e2e.git';
 // nvm is a shell function, must be sourced before use
-const installCmd = `. "${process.env.HOME}/.nvm/nvm.sh" && nvm use 20.18.0 && pnpm i && pnpm build`;
+const installCmd = `pnpm i && pnpm build`;
 
 if (existsSync(join(e2eDir, '.git'))) {
     console.log('[sync-e2e] Pulling latest e2e repo...');
